5a5a2290b8eb98a8049ace7a47e0cf483662aa10
Nelson Gacheru <nelsonzachariah@gmail.com>
2019-06-21 15:43:16 +0300

    angular practise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,12 @@ import {Goal} from './goal'
 })
 export class AppComponent {
   goals = [
-    new Goal(1, 'Watch Finding Nemo'),
-    new Goal(2, 'Buy Cookies'),
-    new Goal(3, 'Get a new Phone Case'),
-    new Goal(4, 'Get Dog Food'),
-    new Goal(5, 'Solve math homewrok'),
-    new Goal(6, 'Plot my world domination plan'),
+    new Goal(1, 'Watch Finding Nemo', 'Find an online version and watch merlin find his son', new Date(2020, 3, 14)),
+    new Goal(2, 'Buy Cookies', 'I have to buy cookies for the parrot', new Date(2020, 6, 9)),
+    new Goal(3, 'Get new Phone Case', 'Diana has her birthday coming up soon', new Date(2020, 1, 12)),
+    new Goal(4, 'Get Dog Food', 'Pupper likes expensive sancks', new Date(2020, 0, 18)),
+    new Goal(5, 'Solve math homework', 'Damn Math', new Date(2020, 2, 14)),
+    new Goal(6, 'Plot my world domination plan', 'Cause I am an evil overlord', new Date(2020, 3, 14)),
+
   ]
 }
